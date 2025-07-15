2641f80f9c911c7ea4f09ae4ff383b486bc0f95a
developeruzairsaleem <uzairsaleemdev@gmail.com>
2025-07-14 23:48:49 -0700

    created a dummy dashboard for customer

--- src/app/dashboard/customer/preferences/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProfilePage() {
-    return (
-      <div className="text-white">
-        <h1 className="text-xl font-bold mb-4">Your Profile</h1>
-        <p>Username, email, avatar, etc will be shown here</p>
-      </div>
-    );
-  }
-  
